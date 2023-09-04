083c32d97194ecc8ff9c7b096c5354e6c5c16dff
GodaHaruki <102050420+GodaHaruki@users.noreply.github.com>
2023-09-04 12:46:52 +0900

    Update index.ts

--- index.ts
+++ index.ts
@@ -7,3 +7,8 @@ function doGet (e: GoogleAppsScript.Events.DoGet): void {
 function doPost (e: GoogleAppsScript.Events.DoPost): void {
   handleDoPost(e)
 }
+
+declare let global: any;
+
+global.doPost = doPost
+global.doGet = doGet
